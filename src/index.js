b54b402e08e0bbf783aedf247e925def117186f9
kaniaa-kr <kaniaa.kirani@gmail.com>
2026-02-25 04:41:57 +0000

    update index

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Halo, ${name}! Selamat belajar TypeScript.`;
+}
+const namaMahasiswa = "Budi";
+console.log(greet(namaMahasiswa));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function hello(nama: string, umur: number): string {
+    return `Selamat datang ${nama} di PWL, sekarang kamu berumur ${umur} tahun.`;
+}
+
+const mahasiswa = "Anakin";
+const umur = 21;
+console.log(hello(mahasiswa, umur));
